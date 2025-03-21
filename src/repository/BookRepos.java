e4aa052334a865dfb01b5f5974dc63b3b230c54e
Andrii-Nehrii <negriyandriy@gmail.com>
2025-03-21 12:48:01 +0100

    add

--- src/repository/BookRepos.java
+++ src/repository/BookRepos.java
@@ -1,6 +1,15 @@
 package repository;
 
+import model.Book;
+import ulils.MyList;
+
 public interface BookRepos {
-    //
+    Book addBook(String title , String author , int id);
+
+    MyList<Book> getAllBooks();
+    Book getByTitle (String title);
+    Book getByAuthor (String author);
+    Book getById (int id);
+    MyList<Book> getFreeBooks ();
 
 }
