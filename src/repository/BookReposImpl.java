dfad398fe3c33cdc1d541c9cbefa492c3ada36e2
Andrii-Nehrii <negriyandriy@gmail.com>
2025-03-24 11:04:17 +0100

    Book Repos , Main interface

---
+++ src/repository/BookReposImpl.java
@@ -0,0 +1,77 @@
+package repository;
+
+import model.Book;
+import ulils.MyArrayList;
+import ulils.MyList;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class BookReposImpl implements BookRepos {
+    private final MyList<Book> books;
+    private final AtomicInteger currentId = new AtomicInteger(1);
+
+    public BookReposImpl(MyList<Book> books) {
+        this.books = new MyArrayList<>();
+    }
+
+    @Override
+    public Book addBook(String title, String author) {
+        Book book = new Book(title, author, currentId.getAndIncrement());
+        books.add(book);
+        return book;
+    }
+
+    @Override
+    public MyList<Book> getAllBooks() {
+        return books;
+    }
+
+    @Override
+    public Book getByTitle(String title) {
+        for (Book b : books) {
+            if (b.getTitle().contains(title)) {
+                return b;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public MyList<Book> getByAuthor(String author) {
+        MyList<Book> result = new MyArrayList<>();
+        for (Book b : books) {
+            if (b.getAuthor().equals(author)) {
+                result.add(b);
+
+            }
+        }
+        return result;
+
+    }
+
+    @Override
+    public Book getById(int id) {
+        for (Book b : books) {
+            if (b.getId() == id) {
+                return b;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public MyList<Book> getFreeBooks() {
+        return null;
+    }
+
+    @Override
+    public void deleteBookById(int id) {
+        for (Book b : books) {
+            if (b.getId() == id) {
+                books.remove(id);
+            }
+        }
+
+
+    }
+}
