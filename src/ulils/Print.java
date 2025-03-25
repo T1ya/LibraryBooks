a627c4c58d6929e86353d60f0fd22f2873c0c66b
Andrii-Nehrii <negriyandriy@gmail.com>
2025-03-25 11:57:16 +0100

    Colors

--- src/ulils/Print.java
+++ src/ulils/Print.java
@@ -1,11 +1,16 @@
 package ulils;
 
+
 public class Print {
+    public static final String RESET = "\u001B[0m";  // Сброс цвета
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
     public static void Error(String message) {
-        System.out.println("Error: " + message);
+        System.out.println(RED +  "Error: " + message + RESET);
     }
 
     public static void Success(String message) {
-        System.out.println("Success: " + message);
+        System.out.println(GREEN +"Success: " + message + RESET);
     }
+
 }
